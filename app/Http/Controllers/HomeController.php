8e725944934b1cef1617eeee7582a5a7aa6c81b9
Ragamaffin <iuylskiy@gmail.com>
2021-05-13 20:05:14 +0300

    pofixil govniwe ;D

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use App\Models\UserRole;
 
 class HomeController extends Controller
 {
@@ -26,15 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $users = User::with('role')->get();
-        $user_role = new UserRole();
-        foreach ($users as $user){
-            if ($user->id == Auth::user()->id) {
-                $user_role = $user;
-            }
-        }
+        $user_role = User::find(1)->role->where('id', Auth::user()->id)->first();
 
-        unset($users);
         return view('home',['user_role' => $user_role]);
     }
 }
